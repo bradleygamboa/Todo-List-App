fd08b05b91fd97e5490f6c63d7bfc15c51c7d864
Bradley Gamboa <bradleygamboa@gmail.com>
2017-01-06 14:14:09 -0600

    added add task

--- app.js
+++ app.js
@@ -2,40 +2,54 @@ var express = require('express');
 var PORT = process.env.PORT || 8080;
 var app = express();
 var Sequelize = require('sequelize');
+
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+
 var models = require('./models');
+models.TodoItem.sync({ force: true }).then(function() {
+        return models.TodoItem.bulkCreate(
+            
+            [{
+                task: "watch rouge one",
+                done: false
+            }, {
+                task: "bathe dog",
+                done: false
+            }, {
+                task: 'brush cat',
+                done: false
+            }]
 
-models.TodoItem.bulkCreate(
-[{
-	task: "watch rouge one",
-	done: false
-},
-{
-	task: "bathe dog",
-	done: false
-},
-{
-	task: 'brush cat',
-	done: false
-}]
+        );
+});
 
-);
 
 app.get('/', function(req, res){
 	//Grabs all todos
 	//Select all todo items
-	// var todoItems = models.TodoItem.findAll({}).then(function(data){
-	// 	res.send (data);
-	// })
-	res.render('home');
+	models.TodoItem.findAll({}).then(function(data){
+		console.log(data)
+		res.render('home', {tasks: data});
+	})
+
 });
 
 app.post('/todos/', function(req, res){
 	//Adds a new todo
-	res.send('Add');
+	console.log(req.body.newTask);
+	models.TodoItem.create({
+		task: req.body.newTask,
+		done: false
+	}).then(function(){
+		res.redirect('/');
+	});
+	
+	
 
 });
 
